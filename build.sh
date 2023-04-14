b15ed6933b952216b40da73c2067d73409666e30
miguel <miguel.mora@solucionesbolivar.com-at-219131814945>
2023-04-14 16:54:57 -0500

    Deploy configurations

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 # exit on error
 set -o errexit
 
-#poetry install
+#pip update and install
+pip install --upgrade pip
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
